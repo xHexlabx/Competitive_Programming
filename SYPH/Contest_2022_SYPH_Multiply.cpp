27409f1c47b8ba82246fc0beaff087970cf54669
Meaw_zap <118115127+xHexlabx@users.noreply.github.com>
2023-01-08 22:28:09 +0700

    .

---
+++ SYPH/Contest_2022_SYPH_Multiply.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std ;
+
+int main(){
+
+    ios_base :: sync_with_stdio(0) , cin.tie(0) ;
+
+    long long a , b ; cin >> a >> b ;
+    cout << a * b ;
+    
+    return 0 ;
+}
