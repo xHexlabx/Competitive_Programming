105217f022bd5cfa0fad6825743fba28cf65b5f6
Meaw_zap <118115127+xHexlabx@users.noreply.github.com>
2023-01-11 21:10:46 +0700

    .

--- TOI/TOI7/toi7_pair.cpp
+++ TOI/TOI7/toi7_pair.cpp
@@ -1,44 +1,54 @@
-#include<bits/stdc++.h>
-#define ull unsigned long long 
+#include <bits/stdc++.h>
+#define ull unsigned long long
 using namespace std;
-bool cmp(pair<ull , int>a , pair<ull , int>b){
-    return a.second < b.second ;
+bool cmp(pair<ull, int> a, pair<ull, int> b)
+{
+    return a.second < b.second;
 }
-const int N = 100001 ;
-class Fenwick {
-    public:
-        ull fw[2][N] ;
-        void update(int idx , ull val){
-            for(; idx < N ;idx += (idx & -idx)){
-                fw[0][idx] += val ;
-                fw[1][idx] ++ ;
-            }
+const int N = 100001;
+class Fenwick
+{
+public:
+    ull fw[2][N];
+    void update(int idx, ull val)
+    {
+        for (; idx < N; idx += (idx & -idx))
+        {
+            fw[0][idx] += val;
+            fw[1][idx]++;
         }
-        pair<ull , int> query(int idx){
-            ull res = 0 ;
-            int cnt = 0 ;
-            for(; idx > 0 ; idx -= (idx & -idx)){
-                res += fw[0][idx] ;
-                cnt += fw[1][idx] ;
-            }
-            return {res , cnt};
-        }
-}fw;
-pair<ull , int>arr[N] ;
-int main(){
-    ios_base :: sync_with_stdio(0) , cin.tie(0);
-    int n  ; cin >> n ;
-    ull res = 0 ;
-    for(int i = 0 ; i < n ; i ++ ){
-        cin >> arr[i].first >> arr[i].second ;
     }
-    
-    sort(arr ,arr + n , cmp);
-    for(int i = n - 1 ; i >= 0 ; i -- ){
-        auto temp = fw.query(arr[i].first) ;
-        res += temp.first + temp.second * arr[i].first ;
+    pair<ull, int> query(int idx)
+    {
+        ull res = 0;
+        int cnt = 0;
+        for (; idx > 0; idx -= (idx & -idx))
+        {
+            res += fw[0][idx];
+            cnt += fw[1][idx];
+        }
+        return {res, cnt};
+    }
+} fw;
+pair<ull, int> arr[N];
+int main()
+{
+    ios_base ::sync_with_stdio(0), cin.tie(0);
+    int n;
+    cin >> n;
+    ull res = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i].first >> arr[i].second;
+    }
+
+    sort(arr, arr + n, cmp);
+    for (int i = n - 1; i >= 0; i--)
+    {
+        auto temp = fw.query(arr[i].first);
+        res += temp.first + temp.second * arr[i].first;
         fw.update(arr[i].first, arr[i].first);
     }
-    cout << res ;
-    return 0 ;
+    cout << res;
+    return 0;
 }
--- TOI/TOI7/toi7_space.cpp
+++ TOI/TOI7/toi7_space.cpp
@@ -1,27 +1,33 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int n ; 
-void solve(string s){
-    if(s.length() == n){
-        for(int i = n - 1 ; i >= 0 ; i -- ){
-            if(s[i] == '0'){
-                cout << s << ' ' ;
-                s[i] = '1' ;
+int n;
+void solve(string s)
+{
+    if (s.length() == n)
+    {
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (s[i] == '0')
+            {
                 cout << s << ' ';
-                s[i] = '0' ;
-                cout << '\n' ;
+                s[i] = '1';
+                cout << s << ' ';
+                s[i] = '0';
+                cout << '\n';
             }
         }
     }
-    else {
+    else
+    {
         solve(s + '0');
         solve(s + '1');
     }
 }
-int main(){
-    ios_base :: sync_with_stdio(0) , cin.tie(0) ;
-    cin >> n ;
+int main()
+{
+    ios_base ::sync_with_stdio(0), cin.tie(0);
+    cin >> n;
     solve("");
-    
-    return 0 ;
+
+    return 0;
 }
