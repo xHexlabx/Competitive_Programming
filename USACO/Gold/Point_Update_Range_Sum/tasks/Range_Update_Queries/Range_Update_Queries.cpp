a0f1f6bc5f67ccfd8dc83e4f13902aea5ddf3189
xhexlabx <tee.hemjinda.work@gmail.com>
2024-08-15 16:19:04 +0700

    .

--- USACO/Gold/Point_Update_Range_Sum/tasks/Range_Update_Queries/Range_Update_Queries.cpp
+++ USACO/Gold/Point_Update_Range_Sum/tasks/Range_Update_Queries/Range_Update_Queries.cpp
@@ -2,9 +2,58 @@
 
 using namespace std ;
 
+const int Size = 2e5 + 1 ;
+
+int n , q , mode , a , b , u , k , val ;
+struct Fenwick {
+
+    long long fw[Size] ;
+
+    void set (int idx , long long val){
+
+        for(; idx < Size ; idx += (idx & -idx)){
+            fw[idx] += val ;
+        }
+        return ;
+    }
+
+    long long get (int idx){
+
+        long long res = 0 ;
+
+        for(; idx > 0 ; idx -= (idx & -idx)){
+            res += fw[idx] ;
+        }
+
+        return res ;
+    }
+
+}fw ;
+
 int main(){
 
     ios_base :: sync_with_stdio(0) , cin.tie(0) ;
+
+    cin >> n >> q ;
+
+    for(int i = 1 ; i <= n ; i ++ ){
+        cin >> val ;
+        fw.set(i , val) ;
+        fw.set(i + 1 , -val) ;
+    }
+
+    while(q -- ){
+        cin >> mode ;
+        if(mode == 1){
+            cin >> a >> b >> u ;
+            fw.set(a , u) ;
+            fw.set(b + 1 , -u) ;
+        }
+        else {
+            cin >> k ;
+            cout << fw.get(k) << '\n';
+        }
+    }
 
     return 0 ;
 }
